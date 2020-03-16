148251cae7dbf484a7c1d4988cbd21cee590fca5
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-03-16 23:06:43 +0000

    convert decimal to binary

--- _bin.c
+++ _bin.c
@@ -1,20 +1,27 @@
 #include "holberton.h"
 
 /**
-* bin - writes the character c to stdout
-* @b: The pointer to print
+* bin - convert the input into binary
+* @b: The input number
 *
 * Return: On success 1.
 * On error, -1 is returned, and errno is set appropriately.
 */
 int _bin(int b)
 {
-	int i = 0;
+	int i = 0, j;
+	char p[100];
 	while (b > 0)
 	{
-		/*p[i] = b % 2;*/
+		p[i] = b % 2;
 		b /= 2;
 		i++;
+	}
+	j = i -1;
+	while (j >= 0)
+	{
+		putchar(p[j] + '0');
+		j--;
 	}
 	return (i);
 }
