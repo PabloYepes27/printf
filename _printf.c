5f092da997312e874d1643f6d84f209c62f725c9
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-03-16 18:07:55 +0000

    _printf function

---
+++ _printf.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+
+int _printf(const char *format, ...)
+{
+	int i = 0, leng = 0, count = 0;
+	va_list args;
+	char *str;
+
+	va_start(args, format);
+	if (!format)
+		return (-1);
+	while (format[i] != '\0')
+	{
+		if (format[i - 1] != '%' && format[i] != '%')
+		{
+			putchar(format[i]);
+		}
+		else
+		{
+			count++;
+			switch (format[i])
+			{
+				case 'c':
+					leng++;
+					putchar((char)va_arg(args, int));
+					break;
+				case 's':
+					str = va_arg(args, char *);
+					if (str == NULL)
+						return (-1);
+					leng += _put(str);
+					break;
+			}
+		}
+		i++;
+	}
+	va_end(args);
+	return (i + leng - count);
+}
