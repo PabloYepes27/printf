d8b6655747dd7585cacd6f29896bfc68b80a6e4f
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-03-16 17:52:22 +0000

    betty bin

--- _bin.c
+++ _bin.c
@@ -2,7 +2,7 @@
 
 /**
 * bin - writes the character c to stdout
-* @bc: The pointer to print
+* @b: The pointer to print
 *
 * Return: On success 1.
 * On error, -1 is returned, and errno is set appropriately.
