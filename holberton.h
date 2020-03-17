9b008f5673d2de94b04b55bc76f1847160b5acba
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-03-17 17:22:36 +0000

    holberton.h

---
+++ holberton.h
@@ -0,0 +1,15 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <limits.h>
+
+int _printf(const char *format, ...);
+int _put(char *c);
+int print_number(int n);
+int _bin(unsigned int b);
+int _other_print(char c, char p);
+#endif
